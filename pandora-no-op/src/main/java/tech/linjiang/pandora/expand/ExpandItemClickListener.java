6667ca2d0f640838537c6c0bace94d2cf705fd63
zhangshiyao <467681837@qq.com>
2022-01-27 17:47:27 +0800

    modify

--- pandora-no-op/src/main/java/tech/linjiang/pandora/expand/ExpandItemClickListener.java
+++ pandora-no-op/src/main/java/tech/linjiang/pandora/expand/ExpandItemClickListener.java
@@ -3,5 +3,5 @@ package tech.linjiang.pandora.expand;
 import android.webkit.WebView;
 
 public interface ExpandItemClickListener {
-    void onExpandItemClick(WebView webView, String content);
+    void onExpandItemClick(WebView webView, String content,String contentType);
 }
